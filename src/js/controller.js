05d0ba02b65878c6dde6f7ecf6f27a39ae661578
unknown <marlonspr@hotmail.com>
2021-05-25 21:59:58 +1200

    webpack & sass

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,13 @@
-console.log();
+// SCSS Webpack
+import "../scss/main.scss";
+
+// Preload
+import { preload } from './utilities/preload';
+
+function init() {
+    preload();
+}
+
+init();
+
+console.log('Hello From Controller');
